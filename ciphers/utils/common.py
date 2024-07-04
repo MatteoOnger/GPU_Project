a610ca1c734b03af2bf88288ab9ac9b393ba280f
MatteoOnger <matteo.onger@studenti.unimi.it>
2024-07-04 10:49:39 +0200

    Updating common.

--- ciphers/utils/common.py
+++ ciphers/utils/common.py
@@ -1,27 +1,19 @@
-"""
-Common utilities for ciphers
+"""Common utilities for ciphers
 """
 
-# ---------------------- FUNCTIONS -------------------------
-def encrypt_1round_feistel(feistel, texts, keys, word_size):
+
+def encrypt_feistel(feistel, texts, keys, word_size):
     feistel(texts, keys, word_size)
     temp = texts[:, 0].copy()
     texts[:, 0] = texts[:, 1]
     texts[:, 1] = temp
 
 
-def decrypt_1round_feistel(feistel, texts, keys, word_size):
+def decrypt_feistel(feistel, texts, keys, word_size):
     temp = texts[:, 0].copy()
     texts[:, 0] = texts[:, 1]
     texts[:, 1] = temp
     feistel(texts, keys, word_size)
-
-
-def use_feistel_schema(
-        texts, keys, number_of_rounds, get_keys_schedule, word_size, operation, rounds_range):
-    keys_schedule = get_keys_schedule(keys, number_of_rounds, word_size)
-    for round_number in rounds_range:
-        operation(texts, keys_schedule[:, round_number], word_size)
 
 
 def get_mask(word_size):
